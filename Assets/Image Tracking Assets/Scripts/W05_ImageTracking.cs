d3b9b496176131c569e98ed0991f3e98b30af42a
EasedMango <joshua.o.m.ross@gmail.com>
2022-11-03 16:41:43 -0400

    Chess

---
+++ Assets/Image Tracking Assets/Scripts/W05_ImageTracking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class W05_ImageTracking : MonoBehaviour
+{
+    ARTrackedImageManager arTrackedImageManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>(); 
+    }
+
+    public void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+
+            if (trackedImage.name == "Logo_NHL")
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = trackedImage.transform.position;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
